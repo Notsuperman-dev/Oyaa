5a8d20681665fdd1ec55736171c5ced67b468287
Notsuperman-dev <supermanonipad@gmail.com>
2024-08-13 20:17:59 +0100

    Update session.js

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,14 +1,26 @@
 // backend/config/session.js
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 const dotenv = require('dotenv');
+const { Pool } = require('pg');
+
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const pgPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false, // For production with Heroku or similar
+});
+
 const sessionMiddleware = session({
+    store: new pgSession({
+        pool: pgPool,                // Connection pool
+        tableName: 'session',        // Use a custom table-name (optional)
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         secure: isProduction,  // Set to true if using HTTPS
         httpOnly: true,        // Helps to prevent cross-site scripting (XSS) attacks
