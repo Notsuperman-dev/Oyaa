aebe0e2a8141eac29e70c0398594f42305684175
Notsuperman-dev <supermanonipad@gmail.com>
2024-08-14 09:04:55 +0100

    Update WorldMessage.js

--- backend/models/WorldMessage.js
+++ backend/models/WorldMessage.js
@@ -10,21 +10,24 @@ const WorldMessage = sequelize.define('WorldMessage', {
     username: {
         type: DataTypes.STRING,
         allowNull: false,
+        set(value) {
+            this.setDataValue('username', value.toLowerCase());
+        }
     },
     expiresAt: {
         type: DataTypes.DATE,
         allowNull: false,
         defaultValue: () => new Date(Date.now() + 60 * 60 * 1000), // 1 hour from now
     },
-    likes: {  // New field for storing the number of likes
+    likes: {
         type: DataTypes.INTEGER,
         defaultValue: 0,
     },
 }, {
     tableName: 'WorldMessages',
-    timestamps: true, // Handles createdAt and updatedAt
+    timestamps: true,
 });
 
-WorldMessage.hasMany(MessageLike, { foreignKey: 'messageId', sourceKey: 'id', onDelete: 'CASCADE' }); // Add this line
+WorldMessage.hasMany(MessageLike, { foreignKey: 'messageId', sourceKey: 'id', onDelete: 'CASCADE' });
 
 module.exports = WorldMessage;
